26aee204fe22c6992edfa0bbeef09df7de06251e
Jordy <rinnn1212@gmail.com>
2024-12-13 16:04:00 +0900

    LHJ : Window Size Modify

--- EngineContents/ContentsCore.cpp
+++ EngineContents/ContentsCore.cpp
@@ -27,7 +27,7 @@ void UContentsCore::EngineStart(UEngineInitData& _Data)
 	// 넌 컨텐츠잖아 엔진이 관리하는 윈도우라는게 존재하는지도 몰라야한다.
 
 	_Data.WindowPos = { 100, 100 };
-	_Data.WindowSize = { 300, 300 };
+	_Data.WindowSize = { 1280, 720 };
 
 	// 윈도우 크기 지정
 }
