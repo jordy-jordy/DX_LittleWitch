d895dbb1555e9eaf238d903a11d0e7175bcb79b7
Jordy <rinnn1212@gmail.com>
2024-12-13 11:05:27 +0900

    LHJ : Window Engine Update

--- EngineCore/EngineCore.cpp
+++ EngineCore/EngineCore.cpp
@@ -1,5 +1,7 @@
 #include "PreCompile.h"
 #include "EngineCore.h"
+#include <EngineBase/EngineDebug.h>
+#include <EnginePlatform/EngineWindow.h>
 
 UEngineCore::UEngineCore()
 {
@@ -9,3 +11,28 @@ UEngineCore::~UEngineCore()
 {
 }
 
+void UEngineCore::EngineStart(HINSTANCE _Instance)
+{
+	UEngineDebug::LeakCheck();
+
+	UEngineWindow::EngineWindowInit(_Instance);
+
+	UEngineWindow MainWindow;
+
+	MainWindow.Open("MainWindow");
+
+	// 윈도우와는 무관합니다.
+	UEngineWindow::WindowMessageLoop(nullptr,
+		[]()
+		{
+
+		});
+
+
+	// 게임 엔진이 시작되었다.
+	// 윈도우창은 엔진이 알아서 띄워줘야 하고.
+
+	// Window 띄워줘야 한다.
+
+
+}
