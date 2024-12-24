c351f1b1bc85bb5d1999be42bbe3c9fc1bfe735c
Jordy <rinnn1212@gmail.com>
2024-12-24 11:55:04 +0900

    LHJ : Add Resources

--- EngineContents/TitleLogo.cpp
+++ EngineContents/TitleLogo.cpp
@@ -6,8 +6,7 @@ ATitleLogo::ATitleLogo()
 {
 	// 랜더러를 만든다.
 	LogoRenderer = CreateDefaultSubObject<USpriteRenderer>();
-	LogoRenderer->SetSprite("Player_Test.png");
-
+	LogoRenderer->SetSprite("Ellie_Basic_Collecting_Mongsiri.png");
 
 	SetActorRelativeScale3D({600.0f, 600.0f, 1.0f });
 	// SetActorLocation({ 200.0f, 100.0f, 1.0f });
