399b55e4617fa81223fab3b3c2d3b5e4dfb37f41
Jordy <rinnn1212@gmail.com>
2024-12-24 18:26:58 +0900

    LHJ : Update

--- EngineContents/TitleLogo.cpp
+++ EngineContents/TitleLogo.cpp
@@ -26,5 +26,5 @@ void ATitleLogo::Tick(float _DeltaTime)
 {
 	AActor::Tick(_DeltaTime);
 
-	// AddActorLocation(FVector{ 0.001f, 0.0f, 0.0f});
+	// AddActorLocation(FVector{ 100.0f * _DeltaTime, 0.0f, 0.0f});
 }
--- EngineCore/EngineCore.cpp
+++ EngineCore/EngineCore.cpp
@@ -4,6 +4,7 @@
 #include <EnginePlatform/EngineWindow.h>
 #include "IContentsCore.h"
 #include "EngineResources.h"
+#include "EngineGUI.h"
 #include "Level.h"
 
 UEngineGraphicDevice UEngineCore::Device;
@@ -11,6 +12,8 @@ UEngineWindow UEngineCore::MainWindow;
 HMODULE UEngineCore::ContentsDLL = nullptr;
 std::shared_ptr<IContentsCore> UEngineCore::Core;
 UEngineInitData UEngineCore::Data;
+UEngineTimer UEngineCore::Timer;
+
 
 std::shared_ptr<class ULevel> UEngineCore::NextLevel;
 std::shared_ptr<class ULevel> UEngineCore::CurLevel = nullptr;
@@ -103,7 +106,7 @@ void UEngineCore::EngineStart(HINSTANCE _Instance, std::string_view _DllName)
 			// 디바이스가 만들어지지 않으면 리소스 로드도 할수가 없다.
 			// 여기부터 리소스 로드가 가능하다.
 			
-
+			UEngineGUI::Init();
 		},
 		[]()
 		{
@@ -169,14 +172,23 @@ void UEngineCore::EngineFrame()
 
 		CurLevel->LevelChangeStart();
 		NextLevel = nullptr;
+		Timer.TimeStart();
 	}
 
-	CurLevel->Tick(0.0f);
-	CurLevel->Render(0.0f);
+	Timer.TimeCheck();
+	float DeltaTime = Timer.GetDeltaTime();
+
+	CurLevel->Tick(DeltaTime);
+	CurLevel->Render(DeltaTime);
+	// GUI랜더링은 기존 랜더링이 다 끝나고 해주는게 좋다.
+
 }
 
 void UEngineCore::EngineEnd()
 {
+
+	UEngineGUI::Release();
+
 	// 리소스 정리도 여기서 할겁니다.
 	Device.Release();
 
