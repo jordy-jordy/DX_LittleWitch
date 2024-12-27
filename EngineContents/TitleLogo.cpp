f249f9490ef283372acc41eda8ec6be61910a21f
Jordy <rinnn1212@gmail.com>
2024-12-27 17:51:12 +0900

    이현정 : 애니메이션 테스트

--- EngineContents/TitleLogo.cpp
+++ EngineContents/TitleLogo.cpp
@@ -8,8 +8,14 @@ ATitleLogo::ATitleLogo()
 {
 	// 랜더러를 만든다.
 	LogoRenderer = CreateDefaultSubObject<USpriteRenderer>();
-	LogoRenderer->SetSprite("Aurea_Idle.png", 3);
+	LogoRenderer->SetSprite("Aurea_Idle.png", 1);
 	LogoRenderer->SetRelativeScale3D({ 500, 500, 1.0f });
+
+	LogoRenderer->CreateAnimation("Idle", "Aurea_Idle.png", 2, 4, 0.15f);
+	USpriteRenderer::FrameAnimation* Animation = LogoRenderer->FindAnimation("Idle");
+	Animation->IsAutoScale = true;
+	Animation->AutoScaleRatio = 4.0f;
+	LogoRenderer->ChangeAnimation("Idle");
 }
 
 ATitleLogo::~ATitleLogo()
