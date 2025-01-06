0e77d72f163cf26d12e99d45815931353bba9081
Jordy <rinnn1212@gmail.com>
2025-01-06 12:21:03 +0900

    이현정 : 엘리 스프라이트 설정

--- EngineContents/Player.cpp
+++ EngineContents/Player.cpp
@@ -23,11 +23,11 @@ APlayer::APlayer()
 	////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////// 郡府 HAT IDLE
 	ELLIE_HAT = CreateDefaultSubObject<USpriteRenderer>();
-	ELLIE_HAT->CreateAnimation("HAT_IDLE_FRONT_LEFT",  "Ellie_Basic_Idle.png", { 27, 28, 28, 29 }, { 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_IDLE_FRONT",       "Ellie_Basic_Idle.png", { 30, 31, 32, 33 }, { 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_IDLE_FRONT_RIGHT", "Ellie_Basic_Idle.png", { 34, 35, 35, 36 }, { 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_IDLE_BACK_LEFT",   "Ellie_Basic_Idle.png", { 37, 38, 38, 37 }, { 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_IDLE_BACK_RIGHT",  "Ellie_Basic_Idle.png", { 39, 40, 40, 39 }, { 0.2f, 0.2f, 0.2f, 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_IDLE_FRONT_LEFT",  "Ellie_Basic_Idle.png", { 27, 28, 28, 29 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_IDLE_FRONT",       "Ellie_Basic_Idle.png", { 30, 31, 32, 33 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_IDLE_FRONT_RIGHT", "Ellie_Basic_Idle.png", { 34, 35, 35, 36 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_IDLE_BACK_LEFT",   "Ellie_Basic_Idle.png", { 37, 38, 38, 37 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_IDLE_BACK_RIGHT",  "Ellie_Basic_Idle.png", { 39, 40, 40, 39 }, { 0.2f });
 
 	////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////// 郡府 SHADOW IDLE
@@ -39,36 +39,36 @@ APlayer::APlayer()
 	//////////////////////////////////////////////////////////////////////////////////// 郡府 WALK
 	ELLIE->CreateAnimation("ELLIE_WALK_UP",       "Ellie_Basic_Walk.png", 77, 84, 0.2f);
 	ELLIE->CreateAnimation("ELLIE_WALK_DOWN",     "Ellie_Basic_Walk.png", 41, 48, 0.2f);
-	ELLIE->CreateAnimation("ELLIE_WALK_LEFT",     "Ellie_Basic_Walk.png",  9, 16, 0.2f);
+	ELLIE->CreateAnimation("ELLIE_WALK_LEFT",     "Ellie_Basic_Walk.png", 33, 40, 0.2f);
 	ELLIE->CreateAnimation("ELLIE_WALK_LEFT_UP",  "Ellie_Basic_Walk.png", 65, 72, 0.2f);
 	ELLIE->CreateAnimation("ELLIE_WALK_RIGHT",    "Ellie_Basic_Walk.png", 53, 60, 0.2f);
 	ELLIE->CreateAnimation("ELLIE_WALK_RIGHT_UP", "Ellie_Basic_Walk.png", 89, 97, 0.2f);
 
 	////////////////////////////////////////////////////////////////////////////////////
 	// 	//////////////////////////////////////////////////////////////////////////////////// 郡府 HAT WALK
-	ELLIE_HAT->CreateAnimation("HAT_WALK_DOWN",     "Ellie_Basic_Walk.png", { 106, 104, 105, 106, 104, 105, 106, 104 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	//ELLIE_HAT->CreateAnimation("HAT_WALK_LEFT",     "Ellie_Basic_Walk.png", { 41, 42, 43, 41, 44, 45 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	//ELLIE_HAT->CreateAnimation("HAT_WALK_LEFT_UP",  "Ellie_Basic_Walk.png", { 56, 57, 58, 56, 59, 60 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	//ELLIE_HAT->CreateAnimation("HAT_WALK_RIGHT",    "Ellie_Basic_Walk.png", { 50, 51, 52, 50, 53, 54 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	//ELLIE_HAT->CreateAnimation("HAT_WALK_RIGHT_UP", "Ellie_Basic_Walk.png", { 62, 63, 64, 62, 65, 66 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_WALK_DOWN",     "Ellie_Basic_Walk.png", { 109, 110, 128, 128, 109, 110, 128, 128 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_WALK_LEFT",     "Ellie_Basic_Walk.png", { 107, 108, 99, 99, 107, 108, 99, 99 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_WALK_LEFT_UP",  "Ellie_Basic_Walk.png", 128, 128, 0.2f);
+	ELLIE_HAT->CreateAnimation("HAT_WALK_RIGHT",    "Ellie_Basic_Walk.png", { 114, 115, 116, 116, 114, 115, 116, 116 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_WALK_RIGHT_UP", "Ellie_Basic_Walk.png", 128, 128, 0.2f);
 
 	////////////////////////////////////////////////////////////////////////////////////
 
 	//////////////////////////////////////////////////////////////////////////////////// 郡府 RUN
-	ELLIE->CreateAnimation("ELLIE_RUN_UP",       "Ellie_Basic_Run.png", { /*28,*/ 29, 30, 31, 32, 33, 34 }, { /*0.2f,*/ 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE->CreateAnimation("ELLIE_RUN_DOWN",     "Ellie_Basic_Run.png", { 9, 10, 11, 12, 13, 14 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE->CreateAnimation("ELLIE_RUN_LEFT",     "Ellie_Basic_Run.png", { 3, 4, 5, 6, 7, 8 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE->CreateAnimation("ELLIE_RUN_LEFT_UP",  "Ellie_Basic_Run.png", { 23, 24, 25, 23, 26, 27 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE->CreateAnimation("ELLIE_RUN_RIGHT",    "Ellie_Basic_Run.png", { /*15,*/ 16, 17, 18, 19, 20, 21 }, { /*0.2f,*/ 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE->CreateAnimation("ELLIE_RUN_RIGHT_UP", "Ellie_Basic_Run.png", { 36, 37, 38, 36, 39, 40 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
+	ELLIE->CreateAnimation("ELLIE_RUN_UP",       "Ellie_Basic_Run.png", { 29, 30, 31, 32, 33, 34 }, { 0.2f });
+	ELLIE->CreateAnimation("ELLIE_RUN_DOWN",     "Ellie_Basic_Run.png", { 9, 10, 11, 12, 13, 14 }, { 0.2f });
+	ELLIE->CreateAnimation("ELLIE_RUN_LEFT",     "Ellie_Basic_Run.png", { 3, 4, 5, 6, 7, 8 }, { 0.2f });
+	ELLIE->CreateAnimation("ELLIE_RUN_LEFT_UP",  "Ellie_Basic_Run.png", { 23, 24, 25, 23, 26, 27 }, { 0.2f });
+	ELLIE->CreateAnimation("ELLIE_RUN_RIGHT",    "Ellie_Basic_Run.png", { 16, 17, 18, 19, 20, 21 }, { 0.2f });
+	ELLIE->CreateAnimation("ELLIE_RUN_RIGHT_UP", "Ellie_Basic_Run.png", { 36, 37, 38, 36, 39, 40 }, { 0.2f });
 
 	////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////// 郡府 HAT RUN
-	ELLIE_HAT->CreateAnimation("HAT_RUN_DOWN",     "Ellie_Basic_Run.png", { 46, 47, 47, 46, 48, 48 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_RUN_LEFT",     "Ellie_Basic_Run.png", { 41, 42, 43, 41, 44, 45 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_RUN_LEFT_UP",  "Ellie_Basic_Run.png", { 56, 57, 58, 56, 59, 60 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_RUN_RIGHT",    "Ellie_Basic_Run.png", { 50, 51, 52, 50, 53, 54 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
-	ELLIE_HAT->CreateAnimation("HAT_RUN_RIGHT_UP", "Ellie_Basic_Run.png", { 62, 63, 64, 62, 65, 66 }, { 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_RUN_DOWN",     "Ellie_Basic_Run.png", { 46, 47, 47, 46, 48, 48 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_RUN_LEFT",     "Ellie_Basic_Run.png", { 41, 42, 43, 41, 44, 45 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_RUN_LEFT_UP",  "Ellie_Basic_Run.png", { 56, 57, 58, 56, 59, 60 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_RUN_RIGHT",    "Ellie_Basic_Run.png", { 50, 51, 52, 50, 53, 54 }, { 0.2f });
+	ELLIE_HAT->CreateAnimation("HAT_RUN_RIGHT_UP", "Ellie_Basic_Run.png", { 62, 63, 64, 62, 65, 66 }, { 0.2f });
 
 	////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////// 郡府 SHADOW RUN
@@ -81,9 +81,9 @@ APlayer::APlayer()
 
 	ELLIE_SHADOW->ChangeAnimation("ELLIE_RUN_SHADOW");
 	ELLIE_SHADOW->SetAutoScale(true);
-	ELLIE->ChangeAnimation("ELLIE_WALK_DOWN");
+	ELLIE->ChangeAnimation("ELLIE_WALK_LEFT");
 	ELLIE->SetAutoScale(true);
-	ELLIE_HAT->ChangeAnimation("HAT_WALK_DOWN");
+	ELLIE_HAT->ChangeAnimation("HAT_WALK_LEFT");
 	ELLIE_HAT->SetAutoScale(true);
 	AUREA->ChangeAnimation("AUREA_IDLE_LEFT");
 	AUREA->SetAutoScale(true);
--- EngineContents/Player.h
+++ EngineContents/Player.h
@@ -2,7 +2,7 @@
 #include <EngineCore/Actor.h>
 
 
-enum class PlayerIDLE_Vector
+enum class EllieIDLE_Vector
 {
 	NONE = 0,
 	IDLE_FRONT_LEFT = 1,
@@ -10,7 +10,32 @@ enum class PlayerIDLE_Vector
 	IDLE_FRONT_RIGHT = 3,
 	IDLE_BACK_LEFT = 4,
 	IDLE_BACK = 5,
-	IDLE_BACK_RIGHT = 6
+	IDLE_BACK_RIGHT = 6,
+	MAX
+};
+
+enum class EllieWALK_Vector
+{
+	NONE = 0,
+	WALK_UP = 1,
+	WALK_DOWN = 2,
+	WALK_LEFT = 3,
+	WALK_LEFT_UP = 4,
+	WALK_RIGHT = 5,
+	WALK_RIGHT_DOWN = 6,
+	MAX
+};
+
+enum class EllieRUN_Vector
+{
+	NONE = 0,
+	RUN_UP = 1,
+	RUN_DOWN = 2,
+	RUN_LEFT = 3,
+	RUN_LEFT_UP = 4,
+	RUN_RIGHT = 5,
+	RUN_RIGHT_DOWN = 6,
+	MAX
 };
 
 // Ό³Έν :
