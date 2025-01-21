db4713b0b2a089218e51b44b4fac92bedec6ca28
Jordy <rinnn1212@gmail.com>
2025-01-21 17:11:23 +0900

    이현정 : GlobalVar 설명 주석 추가

--- EngineContents/GlobalVar.h
+++ EngineContents/GlobalVar.h
@@ -2,21 +2,29 @@
 #include <string_view>
 #include <EngineBase/EngineMath.h>
 
+
+// 원본 사이즈에서 곱해지는 비율
 static const float ScaleRatio = 2.8f;
 
+// 시작 모드
 static std::string_view START_MODE = "PLAY";
 
+// 윈도우 창 크기
 static const FVector WINDOW_SIZE = { 1280, 720 };
 static const FVector HALF_WINDOW_SIZE = WINDOW_SIZE * 0.5;
 
+// 엘리 기본 사이즈
 static const FVector ELLIESIZE = { 50.0f, 50.0f, 0.0f };
 static const FVector ELLIESIZE_PLUS = ELLIESIZE * ScaleRatio;
 
+// 엘리 애니메이션 스피드
 static const float ELLIE_ANIMDEFAULT_SPEED = 0.13f;
 
+// 엘리 걷기, 뛰기 스피드
 static float ELLIE_WALK_SPEED = 500.0f;
 static float ELLIE_RUN_SPEED = 800.0f;
 
+// 각 방향 벡터
 static const FVector VECTOR_NONE = { 0.0f, 0.0f, 0.0f };
 static const FVector VECTOR_UP = { 0.0f, 1.0f, 0.0f };
 static const FVector VECTOR_DOWN = { 0.0f, -1.0f, 0.0f };
@@ -27,6 +35,7 @@ static const FVector VECTOR_RIGHT = { 1.0f, 0.0f, 0.0f };
 static const FVector VECTOR_RIGHT_UP = { 1.0f, 1.0f, 0.0f };
 static const FVector VECTOR_RIGHT_DOWN = { 1.0f, -1.0f, 0.0f };
 
+// 필드 정보
 static const FVector GreenFieldSize = { 720.0f , 604.0f , 0.0f };
 static const FVector GreenFieldSize_Plus = GreenFieldSize * ScaleRatio;
 
